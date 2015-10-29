bcdd2e50bf84dddaafe29071788ece5f9e7d790e
Kieran Reynolds <kreynolds@one-k.com>
2015-10-28 18:46:33 -0700

    Add Account info page

---
+++ page-hos-profile-account-info.php
@@ -0,0 +1,10 @@
+<?php 
+/**
+ * Template Name: HOS Profile - Account Info
+ * This template will provide users with login functionality
+ */
+get_header();?>
+<div class="page-main page-account-info">
+	<div></div>
+</div>
+<?php get_footer();?>
