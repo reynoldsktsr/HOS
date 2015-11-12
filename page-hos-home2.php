880878da6274859b7bbdd7855d5d6330f83b2e65
Kieran Reynolds <kreynolds@one-k.com>
2015-11-11 16:03:58 -0800

    Finished basic home page template

--- page-hos-home2.php
+++ page-hos-home2.php
@@ -7,7 +7,7 @@ get_header();?>
 
 		<div class="row section poster" style="background-image:url('<?php echo get_template_directory_uri();?>/img/homeBG1.jpg')">
 			<div class="wrapper">
-				<div class="col-xs-12 centered middle content">
+				<div class="centered middle content">
 					<h1>Connecting People with Jobs</h1>
 					<hr>
 					<p class="subheader">Here is a sample blurb</p>
@@ -41,6 +41,46 @@ get_header();?>
 				</div>
 			</div>
 		</div>
+
+		<div class="row section two-column">
+			<div class="col-xs-12 centered content image-left">
+				<h1>Job Seekers</h1>
+				<div class="col-xs-12 col-sm-6 image-half">
+					<img src="<?php echo get_template_directory_uri();?>/img/homeBG3.jpg">
+				</div>
+				<div class="col-xs-12 col-sm-6 list">
+					<ul>
+						<li><b>Finding the right job</b> can be a long and difficult task</li>
+						<li>We are constantly <b>connecting with hiring managers</b> to make sure we get the latest job opportunities</li>
+						<li>With our industry relations, we get you in front of the decision maker <b>quicker</b></li>
+						<li><b>We meet with every candidate and hiring manager</b> to make sure we provide the best possible match and <b>fit</b></li>
+						<li>Many of our jobs are <b>not</b> even listed on public job boards</li>
+						<li><b>Hiring managers</b> prefer to use us instead of spending time searching through career sites and resumes</li>
+						<li>This is a <b>free</b> service to all of our candidates</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+
+		<div class="row section two-column image-right">
+			<div class="col-xs-12 centered content">
+				<h1>Employers</h1>
+				<div class="col-xs-12 col-sm-8 list">
+					<ol>
+						<li><b>Resume Screening</b>We screen 100's of resumes so that you don't have to</li>
+						<li><b>Phone Interview</b>We pre-screen all of our candidates on the phone</li>
+						<li><b>Skill Assessment</b>Our industry experts evaluate each candidate's industry specific skills</li>
+						<li><b>1 on 1 Interviews</b>We conduct face to face interviews 100% of the time</li>
+						<li><b>Client Consultation</b>We meet with all of our clients so we understand their business</li>
+						<li><b>Quality Control Calls</b>We follow up with clients regularly to make sure they are satisfied with our placements</li>
+					</ol>
+				</div>
+				<div class="col-xs-12 col-sm-4 image-half">
+					<img src="<?php echo get_template_directory_uri();?>/img/jobSeekersBG1.jpg">
+				</div>
+			</div>
+		</div>
+
 	</div>
 </div>
 <?php get_footer();?>
