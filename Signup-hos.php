c46b60f516cdb401dcf1e81d4d2032a08d02e31c
Tennis <tennis@Tenniss-MacBook-Pro.local>
2015-11-13 16:38:46 -0800

    upload picture for login page

--- Signup-hos.php
+++ Signup-hos.php
@@ -3,29 +3,34 @@
 
 get_header();?>
 <style>
-div#signuppage{}
-div#signuppage1 {width:15px; background-color:#3b8ced; margin-left:50px; border-left: 2px solid #3b8ced; border-radius:5px; float:left;}
+div#signuppage{ background-image: <?php echo get_template_directory_uri();?>/url(Albert_Bridge,_London_-_Oct_2012.jpg)}
+div#login{ background-attachment:fixed; background-image:url(Albert_Bridge,_London_-_Oct_2012.jpg);}
+div#signuppage1 {width:35px; background-color:#3b8ced; vertical-align:middle; margin-left:40%; border-left: 2px solid #3b8ced; border-left-radius:20px; float:left;}
 
-.icon{width:25px;}
+.icon{width:35px;}
+.input{height:35px; width:200px;}
 
 </style>
 
 <div id="signuppage">
+
 <div  style="color:blue; width:1024; text-align:center; background-color:#CCC; font-size:50px;">
    START APPLYING TODAY
 </div>
 <form method="post" action="">
 <div id="login">
+<img src="<?php echo get_template_directory_uri();?>/url(Albert_Bridge,_London_-_Oct_2012.jpg)"/>
 
-
+<br />
     <div id="signuppage1">
  <img src="<?php echo get_template_directory_uri();?>/user64pxcopy.png" class="icon"/></div>
-    <input type="text" placeholder="username" name="username" style="height:25px;" />
-    </span><br>
+    <input type="text" placeholder="username" name="username" class="input"/>
+    </span><br><br />
     
     <div id="signuppage1">
  <img src="<?php echo get_template_directory_uri();?>/mail64px.png" class="icon" /></div>
-    <input type="password" placeholder="password" name="pass" style="height:25px;"><br>
+    <input type="password" placeholder="password" name="pass" class="input">
+    <br><br />
     
     <input type="submit" value="Sign Up" style="background-color:#3b8ced; font-size:16px; color:white;">
 </div>
