7907ed636e2970ada8eb7303f715034014db2cfd
Tennis <tennis@Tenniss-MacBook-Pro.local>
2015-11-13 18:42:42 -0800

    signin page update, almost finished

--- Signin-hos.php
+++ Signin-hos.php
@@ -4,12 +4,28 @@
 get_header();?>
 <style>
 div#signuppage{}
-div#signuppage1 {width:35px; color:white; font-size:36px; background-color:#3b8ced; vertical-align:middle; margin-left:40%; border-left: 2px solid #3b8ced; border-left-radius:20px; float:left;}
+
+div#signuppage1 {width:35px; color:white; font-size:25px; background-color:#3b8ced; vertical-align:middle; margin-left:41%; border-left: 2px solid #3b8ced; border-left-radius:20px; float:left;}
+
 div#login{background-image: url(<?php echo get_template_directory_uri();?>/backgroundsignin.png); background-repeat:no-repeat; background-position:center;  -webkit-background-size: cover; -moz-background-size: cover; -o-background-size: cover; background-size: cover; height:400px;}
+
+div#header{color:white; text-align:center; font-size:50px;}
+
+div#link{color:white; text-align:center; font-size:25px; float:left; margin-left:21%;}
 
 .icon{width:35px;}
 .input{height:35px; width:200px;}
-.submit{background-color:#3b8ced; font-size:16px; color:white; height:35px; width:200px; margin-left:42%;
+.submit{background-color:#3b8ced; font-size:16px; color:white; height:35px; width:200px; margin-left:42%;}
+
+a:link, a:visited {
+    color: white;
+    text-align: center;
+    text-decoration: none;
+}
+
+a:hover, a:active {
+    color: blue;
+}
 
 </style>
 
@@ -17,9 +33,11 @@ div#login{background-image: url(<?php echo get_template_directory_uri();?>/backg
 
 <form method="post" action="">
 <div id="login">
+<div id="header"> 
 HiredOn
+</div>
 
-<br /><br /><br /><br /><br />
+<br /><br /><br />
     <div id="signuppage1">
  <img src="<?php echo get_template_directory_uri();?>/user64pxcopy.png" class="icon"/></div>
     <input type="text" placeholder="username" name="username" class="input"/>
@@ -31,6 +49,14 @@ HiredOn
     <br><br />
     
     <input type="submit" value="Login" class="submit">
+    <br/> <br /><br /><br />
+    <div id="link">
+ <a href=" " target=" k">Forgot Username</a> 
+ 
+	</div>
+    <div id="link">
+    <a href=" " target=" ">Forgot Password</a>
+    </div>
 </div>
 
 </form>
