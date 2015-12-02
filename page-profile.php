56608fd7fca65ede9cb83803ec9bd18f456f05c9
Kieran Reynolds <kreynolds@one-k.com>
2015-12-02 15:27:24 -0800

    Fix bug

--- page-profile.php
+++ page-profile.php
@@ -57,43 +57,43 @@ if(isset($_GET['video_link'])) {
         $professional_title = $_GET['professional_title'];
         $current_user = wp_get_current_user();
         $id = $current_user->ID;
-        $wpdb->query("UPDATE wp_hos_users SET professional_title ='$professional_title' WHERE id='$id' ");
+        $wpdb->query("UPDATE wp_users SET professional_title ='$professional_title' WHERE id='$id' ");
    }
    if(isset($_GET['hourly_rate'])) {
         $hourly_rate = $_GET['hourly_rate'];
         $current_user = wp_get_current_user();
         $id = $current_user->ID;
-        $wpdb->query("UPDATE wp_hos_users SET hourly_rate ='$hourly_rate' WHERE id='$id' ");
+        $wpdb->query("UPDATE wp_users SET hourly_rate ='$hourly_rate' WHERE id='$id' ");
    }
     if(isset($_GET['country'])) {
         $country = $_GET['country'];
         $current_user = wp_get_current_user();
         $id = $current_user->ID;
-        $wpdb->query("UPDATE wp_hos_users SET country ='$country' WHERE id='$id' ");
+        $wpdb->query("UPDATE wp_users SET country ='$country' WHERE id='$id' ");
    }
    if(isset($_GET['category'])) {
         $category = $_GET['category'];
         $current_user = wp_get_current_user();
         $id = $current_user->ID;
-        $wpdb->query("UPDATE wp_hos_users SET category ='$category' WHERE id='$id' ");
+        $wpdb->query("UPDATE wp_users SET category ='$category' WHERE id='$id' ");
    }
    if(isset($_GET['skill'])) {
         $skill = $_GET['skill'];
         $current_user = wp_get_current_user();
         $id = $current_user->ID;
-        $wpdb->query("UPDATE wp_hos_users SET skill ='$skill' WHERE id='$id' ");
+        $wpdb->query("UPDATE wp_users SET skill ='$skill' WHERE id='$id' ");
    }
    if(isset($_GET['years_experience'])) {
         $years_experience = $_GET['years_experience'];
         $current_user = wp_get_current_user();
         $id = $current_user->ID;
-        $wpdb->query("UPDATE wp_hos_users SET years_experience ='$years_experience' WHERE id='$id' ");
+        $wpdb->query("UPDATE wp_users SET years_experience ='$years_experience' WHERE id='$id' ");
    }
    if(isset($_GET['about_me'])) {
         $about_me = $_GET['about_me'];
         $current_user = wp_get_current_user();
         $id = $current_user->ID;
-        $wpdb->query("UPDATE wp_hos_users SET about_me ='$about_me' WHERE id='$id' ");
+        $wpdb->query("UPDATE wp_users SET about_me ='$about_me' WHERE id='$id' ");
    }
 
 ?>
